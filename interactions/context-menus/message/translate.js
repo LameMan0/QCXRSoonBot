ab769e5d3c744a25c05fa60c52e90a3cd935a106
LameMan0 <81112532+LameMan0@users.noreply.github.com>
2023-05-28 23:30:20 -0500

    replace translator AGAIN
    this time with the superior gpt-3.5-turbo

--- interactions/context-menus/message/translate.js
+++ interactions/context-menus/message/translate.js
@@ -1,46 +1,44 @@
-/**
- * @type {import('../../../typings').ContextInteractionCommand}
- */
-
-
-const { EmbedBuilder } = require('discord.js');
-const fetch = require('node-fetch');
-const apiKey = 'YOUR_DEEPL_API_KEY';
+const { ContextMenuInteraction, MessageEmbed } = require('discord.js');
+const openai = require('openai');
 
 module.exports = {
-  data: {
-    name: 'Translate',
-    type: 3, // 3 is for message context menus
-  },
+  name: 'Translate',
+  type: 'MESSAGE',
 
   async execute(interaction) {
-    const { channel, targetId } = interaction;
+    if (!(interaction instanceof ContextMenuInteraction)) {
+      return;
+    }
 
-    const query = await channel.messages.fetch(targetId);
-    const raw = query.content;
+    const { content } = await interaction.target.fetch();
+    const translatedText = await translateText(content);
 
-    const response = await fetch(`https://api-free.deepl.com/v2/translate?auth_key=${apiKey}&text=${encodeURIComponent(raw)}&target_lang=en`);
-    const data = await response.json();
-    const translated = data.translations[0].text;
+    const embed = new MessageEmbed()
+      .setColor('BLUE')
+      .setTitle('Translated to English Language')
+      .addFields(
+        {
+          name: 'Your text:',
+          value: `\`\`\`${content}\`\`\``
+        },
+        {
+          name: 'Translated text:',
+          value: `\`\`\`${translatedText}\`\`\``
+        }
+      )
+      .setFooter({
+        text: `Requested by ${interaction.user.username}`,
+        iconURL: interaction.user.displayAvatarURL({ dynamic: true })
+      })
+      .setTimestamp();
 
-    return interaction.reply({
-      embeds: [
-        new EmbedBuilder()
-          .setColor('Blue')
-          .setTitle('Translated to English Language')
-          .addFields(
-            {
-              name: 'Your text:',
-              value: `\`\`\`${raw}\`\`\``,
-            },
-            {
-              name: 'Translated text:',
-              value: `\`\`\`${translated}\`\`\``,
-            }
-          )
-          .setFooter({ text: `Requested by ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL({ dynamic: true }) })
-          .setTimestamp(),
-      ],
-    });
-  },
+    await interaction.reply({ embeds: [embed] });
+  }
 };
+
+async function translateText(textToTranslate) {
+  const openaiClient = new openai.LanguageModelApi('YOUR_OPENAI_API_KEY');
+  const prompt = `Translate "${textToTranslate}" to English`;
+  const response = await openaiClient.complete(prompt, ['\n'], { model: 'gpt-3.5-turbo', maxTokens: 200 });
+  return response.choices[0].text.trim();
+}
