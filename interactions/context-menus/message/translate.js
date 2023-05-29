823e67af652282ca02b11388b0bcddfc84c634f1
LameMan0 <81112532+LameMan0@users.noreply.github.com>
2023-05-28 21:25:56 -0500

    doesn't seem to work yet but i'm doing this to get a better representation of why it doesn't

--- interactions/context-menus/message/translate.js
+++ interactions/context-menus/message/translate.js
@@ -2,42 +2,45 @@
  * @type {import('../../../typings').ContextInteractionCommand}
  */
 
-const {EmbedBuilder} = require("discord.js");
 
-const translate = require("@iamtraction/google-translate");
+const { EmbedBuilder } = require('discord.js');
+const fetch = require('node-fetch');
+const apiKey = 'YOUR_DEEPL_API_KEY';
 
 module.exports = {
-    data: {
-        name: "Translate",
-        type: 3, // 3 is for message context menus
-    },
+  data: {
+    name: 'Translate',
+    type: 3, // 3 is for message context menus
+  },
 
-    async execute(interaction) {
-        const { channel, targetId } = interaction
+  async execute(interaction) {
+    const { channel, targetId } = interaction;
 
-        const query = await channel.messages.fetch(targetId)
-        const raw = query.content
+    const query = await channel.messages.fetch(targetId);
+    const raw = query.content;
 
-        const translated = await translate(query, { to: "en" })
+    const response = await fetch(`https://api-free.deepl.com/v2/translate?auth_key=${apiKey}&text=${encodeURIComponent(raw)}&target_lang=en`);
+    const data = await response.json();
+    const translated = data.translations[0].text;
 
-        return interaction.reply({
-            embeds: [
-                new EmbedBuilder()
-                    .setColor("Blue")
-                    .setTitle(`Translated to English Language`)
-                    .addFields(
-                        {
-                            name: `Your text:`,
-                            value: `\`\`\`${raw}\`\`\``,
-                        },
-                        {
-                            name: `Translated text:`,
-                            value: `\`\`\`${translated.text}\`\`\``,
-                        }
-                    )
-                    .setFooter({ text: `Requested by ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL(true) })
-                    .setTimestamp()
-            ]
-        })
-    }
-}
+    return interaction.reply({
+      embeds: [
+        new EmbedBuilder()
+          .setColor('Blue')
+          .setTitle('Translated to English Language')
+          .addFields(
+            {
+              name: 'Your text:',
+              value: `\`\`\`${raw}\`\`\``,
+            },
+            {
+              name: 'Translated text:',
+              value: `\`\`\`${translated}\`\`\``,
+            }
+          )
+          .setFooter({ text: `Requested by ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL({ dynamic: true }) })
+          .setTimestamp(),
+      ],
+    });
+  },
+};
