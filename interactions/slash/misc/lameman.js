76284b29ed67a093d4a46f977eb05bef5413e9fb
LameMan0 <81112532+LameMan0@users.noreply.github.com>
2023-05-28 20:47:28 -0500

    Create lameman.js
    :D

---
+++ interactions/slash/misc/lameman.js
@@ -0,0 +1,13 @@
+/**
+ * @type {import('../../../typings').SlashInteractionCommand}
+ */
+
+module.exports = {
+    data: new SlashCommandBuilder()
+      .setName('lameman')
+      .setDescription('lameman'),
+  
+    async execute(interaction) {
+		message.channel.send({ content: "lameman..." });
+	},
+};
